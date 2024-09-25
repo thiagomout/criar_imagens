7687ccba480a908fa4e8abd6d2c80ee1df6f5b81
Thiago Moutinho <thiagomoutinho19@gmail.com>
2024-09-25 14:13:14 -0300

    changed interpolation method and escala is now an argument

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Um script em Python para recortar o contorno de uma imagem e colar em outra.
 
 1. Navegue até o diretório do projeto:
     ```bash
-    cd criar_imagens
+     cd criar_imagens
     ```
 
-2. Execute o script:
+2. Execute o script com o argumento de escala:
     ```bash
-    python3 criandoimagens.py
+    python3 criandoimagens.py <escala>
     ```
 
-Quando executado, o programa perguntará a escala da imagem a ser colada. Recomendo usar `0.1` de acordo com meus testes, mas o valor pode variar de imagem para imagem.
+    Onde `<escala>` é o valor da escala da imagem a ser colada. Recomendo usar `0.07` de acordo com meus testes, mas o valor pode variar de imagem para imagem.
 
 ## Exemplo de Uso
 
@@ -22,4 +22,4 @@ Aqui está um exemplo de como o script pode ser utilizado:
 
 ```bash
 cd criar_imagens
-python3 criandoimagens.py
+python3 criandoimagens.py 0.07
--- criandoimagens.py
+++ criandoimagens.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import random
+import sys
 
 # Carregar a imagem original e a imagem de fundo
-img = cv2.imread('/home/thiago/img_micra/MD022508_training_set/Planktic/MD022508-0-0-Planktic-0005.jpg')
+img = cv2.imread('/home/thiago/img_micra/MD022508_training_set/Planktic/MD022508-0-0-Planktic-0009.jpg')
 fundo = cv2.imread('/home/thiago/img_micra/fundo/istockphoto-1209961067-612x612.jpg')
 
 #blur image
@@ -24,15 +25,18 @@ fundo_redimensionado = cv2.resize(fundo, (img.shape[1], img.shape[0]))
 # Isolar o objeto da imagem original usando a máscara
 img_cortada = cv2.bitwise_and(img, img, mask=mask)
 
+img_cortada = cv2.GaussianBlur(img_cortada, (5, 5), 0)
+
 # Redimensionar a imagem cortada e a máscara
-escala = input("Digite a escala da imagem cortada - Recomendado 0.07\n")
+escala = sys.argv[1]
 escala = float(escala)
 largura = int(img_cortada.shape[1] * escala)
 altura = int(img_cortada.shape[0] * escala)
 dimensao = (largura, altura)
 img_cortada_redimensionada = cv2.resize(img_cortada, dimensao)
-mask_resized = cv2.resize(mask, dimensao, interpolation=cv2.INTER_NEAREST)
-mask_inverted_resized = cv2.resize(mask_inverted, dimensao, interpolation=cv2.INTER_NEAREST)
+print("Dimensão da imagem cortada redimensionada:", img_cortada_redimensionada.shape)
+mask_resized = cv2.resize(mask, dimensao, interpolation=cv2.INTER_LANCZOS4)
+mask_inverted_resized = cv2.resize(mask_inverted, dimensao, interpolation=cv2.INTER_LANCZOS4)
 
 # Garantir que a máscara redimensionada tenha o mesmo número de canais que a imagem
 mask_resized_bgr = cv2.cvtColor(mask_resized, cv2.COLOR_GRAY2BGR)
@@ -42,6 +46,7 @@ mask_inverted_resized_bgr = cv2.cvtColor(mask_inverted_resized, cv2.COLOR_GRAY2B
 x_offset = random.randint(0, 366)
 y_offset = random.randint(0, 366)
 #x_offset, y_offset = 50, 50  # Ajustar conforme necessário
+print(x_offset, y_offset)
 
 # Copiar a imagem cortada redimensionada para o fundo
 fundo_redimensionado[y_offset:y_offset + altura, x_offset:x_offset + largura] = cv2.bitwise_and(fundo_redimensionado[y_offset:y_offset + altura, x_offset:x_offset + largura], mask_inverted_resized_bgr)
